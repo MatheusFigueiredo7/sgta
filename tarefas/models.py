7960f88a0fa14c91b3d645d0eb9aaa3637f47952
Matheus <matheus.figueiredo0620@gmail.com>
2026-03-23 21:31:07 -0300

    Aula 5

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Tarefas(models.Model):
+    status_choices = [
+        ("ABERTA", "Aberta"),
+        ("EM_ANDAMENTO", "Em andamento"),
+        ("CONCLUIDA", "Concluída"),
+        ("CANCELADA", "Cancelada")
+    ]
+    
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    status = models.CharField(max_length=20, choices=status_choices, default="ABERTA")
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_entrega = models.DateField()
+    prioridade = [
+        ("URGENTE", "Urgente"),
+        ("NAO_URGENTE", "Não urgente")
+    ]
+    
+    def __str__(self):
+        return self.titulo
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Tarefas
 
-# Create your views here.
+def listar_tarefas(request):
+    tarefas = Tarefas.objects.all().values()
+    return JsonResponse(list[any](tarefas), safe=False)
+
+def listar_abertas(request):
+    tarefas_abertas = Tarefas.objects.filter(status="ABERTA").values()
+    return JsonResponse(list[any](tarefas_abertas), safe=False)
